938067c5dd8bc98226e3552a9c57f4016730fde5
David Lin <David.Lin@GMR.local>
2019-01-27 16:33:55 -0800

    not really

---
+++ MiniMaxLib/MiniMaxLib/IGameState.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreelessTicTacToe
+{
+    interface IGameState
+    {
+        IGame Game { get; }
+        int Player { get; }
+        int NextPlayer { get; }
+        bool GameOver { get; }
+        IGameState[] Moves();
+        int Score(int player);
+    }
+}
---
+++ MiniMaxLib/MiniMaxLib/Minimax.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreelessTicTacToe
+{
+    class Minimax
+    {
+        public int MiniMax(IGameState state, int currDepth, int maxDepth, int player)
+        {
+            if (state.GameOver || currDepth >= maxDepth)
+            {
+                return state.Score(player);
+            }
+
+            if (player != state.Player)
+            {
+                int maxMoveIndex = state.Moves().Select((x => MiniMax(x, currDepth + 1, maxDepth, player))).Max();
+                return state.Moves()[maxMoveIndex].Score(player);
+            }
+            else
+            {
+                int minMoveIndex = state.Moves().Select((x => MiniMax(x, currDepth + 1, maxDepth, player))).Min();
+                return state.Moves()[minMoveIndex].Score(player);
+            }
+        }
+    }
+}
